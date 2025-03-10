23877af6a8b553d26e44b5cd60688c874c2b46d5
Oguz Turkay <tyserract@gmail.com>
2025-03-10 10:03:15 +0300

    Add database connection utility using Gorm and Postgres

---
+++ internal/db/sqldb.go
@@ -0,0 +1,42 @@
+package db
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var db_ *gorm.DB
+
+// Connect establishes a connection to the database
+func Connect() {
+	dsn := os.Getenv("SUPABASE_DSN")
+
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold: time.Second,
+			Colorful:      false,
+		},
+	)
+
+	var err error
+	db_, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+		PrepareStmt: false,
+		Logger:      newLogger,
+	})
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	fmt.Println("Connected to database")
+}
+
+func GetDB() *gorm.DB {
+	return db_
+}
